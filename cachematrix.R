6c5189497e3263efd843f3e82bdf4ac050128190
rossanor <rossanor@users.noreply.github.com>
2014-10-22 16:29:07 -0200

    Update cachematrix.R
    22/10/2014

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#----------------------------------------------------------------
+# function makeCacheMatrix()
+#
+# It creates a matrix that allows to recover the inverse (function 'solve()')
+# if it was alreary estimated
+#
+# arguments: x = matrix
+#----------------------------------------------------------------
 makeCacheMatrix <- function(x = matrix()) {
-
+        INV <- NULL # set INV = NULL; matriz is being created
+                    # 'x' stores the matriz in the environment inside the object 
+        
+        # functions inside object
+        set <- function(y) {
+                x <<- y
+                INV <<- NULL
+        }
+        get <- function() x
+        setSolve <- function(inv) INV <<- inv
+        getSolve <- function() INV
+        
+        # return result
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
-
+#----------------------------------------------------------------
+# function cacheSolve()
+#
+# It estimates the inverse
+#
+# arguments: x = object createde by makeCacheMatrix() 
+#----------------------------------------------------------------
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Return a matrix that is the inverse of 'x'
+        inv <- x$getSolve()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setSolve(inv)        
+        inv  # return result
 }
